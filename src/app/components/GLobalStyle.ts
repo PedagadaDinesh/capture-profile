ff9f940c3f2a5e3effe1af8928f0fde54fefb7e5
peddagadadinesh2000@gmail.com <peddagadadinesh2000@gmail.com>
2024-01-15 17:30:35 +0530

    Styled Service Section

--- src/app/components/GLobalStyle.ts
+++ src/app/components/GLobalStyle.ts
@@ -10,6 +10,7 @@ const GLobalStyle = createGlobalStyle`
 
    body{
     background: #1b1b1b;
+    font-family: "Inter",sans-serif;
    }
 
    button{
@@ -21,13 +22,14 @@ const GLobalStyle = createGlobalStyle`
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: "Inter",sans-serif;
     &:hover {
         background-color: #23d997;
     }
    }
    h2{
         font-weight: lighter;
-        font-size: 4rem;
+        font-size: 3.5rem;
     }
     h3{
         color:white;
@@ -46,7 +48,7 @@ const GLobalStyle = createGlobalStyle`
     p{
         padding: 3rem 0rem;
         color: #ccc;
-        font-size: 1.4rem;
+        font-size: 1rem;
         line-height: 150%;
     }
 `;
--- src/app/components/ServiceSection.tsx
+++ src/app/components/ServiceSection.tsx
@@ -1,46 +1,82 @@
+"use client";
 import React from "react";
+import styled from "styled-components";
+import { About, Description, Hide, Image } from "../styles";
 
 const ServiceSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> service.
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards className="">
+          <Card>
             <div className="icon">
               <img src="/clock.svg" alt="clock" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src="/teamwork.svg" alt="teamwork" />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src="/diaphragm.svg" alt="diaphragm" />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src="/money.svg" alt="money" />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <img src="/home2.png" alt="home2" />
-    </div>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img src="/home2.png" alt="home2" />
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding-top: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 19rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 60%;
+    h3 {
+      margin-left: true;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServiceSection;
