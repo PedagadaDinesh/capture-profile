945b7f7d085e8e6f3ee464ee9779c39a53574240
peddagadadinesh2000@gmail.com <peddagadadinesh2000@gmail.com>
2024-01-15 16:33:30 +0530

    Styled AboutSection

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -1,31 +1,67 @@
+"use client";
 import React from "react";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div className="description h-screen bg-black text-white">
-      <div className="title">
-        <div className="hide">
-          <h2>We work to make</h2>
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span>
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>come true.</h2>
+          </Hide>
+          <p>
+            Contact us for any photography or videography ideas that you have.
+            We have professionals with amazing skills.
+          </p>
+          <button>Contact us</button>
         </div>
-        <div className="hide">
-          <h2>
-            your <span>dreams</span> come
-          </h2>
-        </div>
-        <div className="hide">
-          <h2>true.</h2>
-        </div>
-        <p>
-          Contact us for any photography or videography ideas that you have. We
-          have professionals with amazing skills.
-        </p>
-        <button>Contact us</button>
-      </div>
-      <div>
+      </Description>
+      <Image>
         <img src="/home1.png" alt="image" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+// Styled Component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 5rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import GLobalStyle from "./components/GLobalStyle";
 
 const page = () => {
   return (
     <div>
+      <GLobalStyle />
       <AboutUs />
     </div>
   );
